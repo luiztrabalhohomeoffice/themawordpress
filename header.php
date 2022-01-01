57a5cda02fcb328b839398263ee5a3ed3a305f6d
Luiz Carlos Junior <luiz.trabalhohomeoffice@gmail.com>
2022-01-01 20:47:32 -0300

    arquivo de rodape

---
+++ header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Curso Wordpress</title>
+</head>
+<body <?php body_class();?>>
+    <p>criando nosso primeiro tema wordpress</p>
+    <header>
+        <section class="top-bar">
+            <div class="social-media-icons"></div>
+            <div class="search"></div>
+        </section> 
+        <section class="menu-area">
+            <section class="logo"></section>
+            <nav class="menu"></nav>
+        </section>  
+
+    </header>   
