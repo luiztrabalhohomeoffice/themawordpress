6d6b274a5c1b5fcb71124636f088cfd0373cfebf
Luiz Carlos Junior <luiz.trabalhohomeoffice@gmail.com>
2022-01-01 20:49:11 -0300

    alterações no arquivo

--- index.php
+++ index.php
@@ -1,12 +1,17 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Curso Wordpress</title>
-</head>
-<body>
-    <p>criando nosso primeiro tema wordpress</p>
-</body>
-</html>
+<?php get_header(); ?>
+    <div class="content-area">
+        <main>
+            <section class="slide"></section>
+            <section class="services"></section>
+            <section class="middle-aarea">
+                <aside class="sidebar"></aside>
+                <div class="news"></div>
+
+            </section>
+            <section class="map"></section>
+        </main>   
+
+    </div>
+    <?php get_footer(); ?>
+    
+   
