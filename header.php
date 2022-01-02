da37724acc482ee0645b568c7528c8408cfe85f7
Luiz Carlos Junior <luiz.trabalhohomeoffice@gmail.com>
2022-01-01 21:12:32 -0300

    insere função wp_head para chamar funções

--- header.php
+++ header.php
@@ -5,16 +5,17 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Curso Wordpress</title>
+    <?php wp_head(); ?>
 </head>
 <body <?php body_class();?>>
     <p>criando nosso primeiro tema wordpress</p>
     <header>
         <section class="top-bar">
-            <div class="social-media-icons"></div>
-            <div class="search"></div>
+            <div class="social-media-icons">icones sociais</div>
+            <div class="search">pesquisa</div>
         </section> 
         <section class="menu-area">
-            <section class="logo"></section>
-            <nav class="menu"></nav>
+            <section class="logo">logo</section>
+            <nav class="menu">menu</nav>
         </section>  
     </header>   
